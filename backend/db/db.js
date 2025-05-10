989d918054400f24b5dcf3f7faee239e6515cc52
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-05-10 16:07:49 +0500

    add some files.

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-// import dotenv from 'dotenv';
-// dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectToMongoDB = async () => {
     try {
         // await mongoose.connect(process.env.MONGO_URI);
-        await mongoose.connect("mongodb://localhost:27017/task_app");
+        // await mongoose.connect("mongodb://localhost:27017/task_app");
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDb")
     } catch (error) {
         console.log("Error connecting to MongoDb", error.message);
--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ import authRouter from "./routes/auth.js"
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: [""],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json())
 app.use("/api/auth", authRouter);
 app.use("/api/task", authRouter);
