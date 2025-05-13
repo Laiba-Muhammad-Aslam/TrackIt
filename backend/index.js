dbbdeb0b9ae9289446ebbae380edd04d2f59814f
Laiba-Muhammad-Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2025-05-13 16:51:39 +0500

    Update frontend URL

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import authRouter from "./routes/auth.js"
 dotenv.config();
 
 const corsOptions ={
-    origin:'http://localhost:5174', 
+    origin:'http://localhost:5173', 
     credentials:true,            
     optionSuccessStatus:200
 }
